82f666d70527c4c018fbc6630a68f83f9187068e
Way Barrios <waybarrios@gmail.com>
2018-10-18 00:14:22 -0400

    load data from UCF101

---
+++ lib/utils/load_data_ucf101.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------
+# Deep Feature Flow
+# Copyright (c) 2017 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Yuwen Xiong
+# --------------------------------------------------------
+
+import numpy as np
+from dataset import *
+from dataset.ucf101 import UCF101
+
+
+def load_gt_imdb(dataset_name, root_path, dataset_path, traintestlist_path, result_path=None, split = '01',
+                  flip=False):
+    """ load ground truth roidb """
+    imdb = eval(dataset_name)(root_path, dataset_path, traintestlist_path, result_path, split)
+    gtdb = imdb.gt_db()
+    if flip:
+        gtdb = imdb.append_flipped_images(gtdb)
+    return gtdb
