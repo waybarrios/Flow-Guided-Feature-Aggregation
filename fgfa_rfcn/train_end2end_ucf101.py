9621052c525b3a6cda2923aba5f0c316ff5b7751
Wayner Barrios <waybarrios@gmail.com>
2018-10-25 11:36:34 -0400

    fixing layer

--- fgfa_rfcn/train_end2end_ucf101.py
+++ fgfa_rfcn/train_end2end_ucf101.py
@@ -66,8 +66,8 @@ def train_net(args, ctx, pretrained, pretrained_flow, epoch, prefix, begin_epoch
     config.symbol = 'resnet_v1_101_flownet_rfcn_ucf101'
     shutil.copy2(os.path.join(curr_path, 'symbols', config.symbol + '.py'), final_output_path)
     sym_instance = eval(config.symbol + '.' + config.symbol)()
-    sym= sym_instance.get_train_heat_flow(config)
-    feat_sym=sym.get_internals()['cam_softmax_output']
+    sym=sym_instance.get_train_heat_flow(config)
+    feat_sym=sym.get_internals()['softmax_output']
 
     # setup multi-gpu
     batch_size = len(ctx)
